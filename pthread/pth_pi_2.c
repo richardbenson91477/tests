39c26e3c2fcf3e8e36d1e761f03d67d32a600583
Richard Benson <richardbenson91477@protonmail.com>
2021-02-10 22:19:44 -0600

    seems to work

--- pthread/pth_pi_2.c
+++ pthread/pth_pi_2.c
@@ -5,9 +5,14 @@
 
 void *pi_thread (void *v);
 
+const int THREADS_N = 4;
 const uint64_t num_steps = 1e+9;
 const double step = 1.0 / (double)num_steps;
-const int THREADS_N = 4;
+
+typedef struct {
+    int t_n;
+    double t_total;
+} t_data_t;
 
 // a way to estimate π c/o https://www.youtube.com/watch?v=FQ1k_YpyG_A
 int main () {
@@ -17,8 +22,11 @@ int main () {
     pthread_attr_init (&attr);
     pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_JOINABLE);
 
+    t_data_t t_data[THREADS_N];
+
     for (int t_c = 0; t_c < THREADS_N; t_c++) {
-        int res = pthread_create (&thread[t_c], &attr, pi_thread, (void *)&t_c);
+        t_data[t_c].t_n = t_c;
+        int res = pthread_create (&thread[t_c], &attr, pi_thread, &t_data[t_c]);
         if (res) {
             printf ("ERROR: pthread_create() returned %d\n", res);
             return -1;
@@ -29,13 +37,12 @@ int main () {
 
     double pi = 0.0;
     for (int t_c = 0; t_c < THREADS_N; t_c++) {
-        double *t_total;
-        int res = pthread_join (thread[t_c], (void **)&t_total);
+        int res = pthread_join (thread[t_c], NULL);
         if (res) {
             printf ("ERROR: pthread_join() returned %d\n", res);
             return -2;
         }
-        pi += step * *t_total;
+        pi += step * t_data[t_c].t_total;
     }
 
     const double pi_valid = 3.141592653589793238;
@@ -44,15 +51,16 @@ int main () {
 }
 
 void *pi_thread (void *v) {
-    int t_start = *(int *)v;
-    double t_total = 0.0;
+    t_data_t *t_data = (t_data_t *)v;
+
+    t_data->t_total = 0.0;
 
     double x;
-    for (uint64_t i = t_start; i < num_steps; i += THREADS_N) {
+    for (uint64_t i = t_data->t_n; i < num_steps; i += THREADS_N) {
         x = (i + 0.5) * step;
-        t_total += 4.0 / (1.0 + x * x);
+        t_data->t_total += 4.0 / (1.0 + x * x);
     }
 
-    pthread_exit((void *)&t_total);
+    pthread_exit(NULL);
 }
 
